b7189e08d55561bdfd0b70f08509db6872fdb361
Huyen Vi <100335881+huyenvi237@users.noreply.github.com>
2022-07-06 15:45:06 +0900

    first commit
    画面ビューを作成

--- lib_sys/src/App.js
+++ lib_sys/src/App.js
@@ -1,10 +1,44 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
+import { 
+  BrowserRouter as Router,
+  Route, Routes
+ } from 'react-router-dom';
+//Style components
+import {StyledContainer} from './components/Style'
+//Loader
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+//LoginView
+import LoginView from './pages/LoginView';
+
+//Test view
+import MemberInfoView from './pages/MemberInfoView';
+import RenewPasswordView from './pages/RenewPasswordView';
+import UserView from './pages/UserView';
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello!</p>
+    <div>
+      <Router>
+        <StyledContainer>
+          <Routes>
+            <Route index element={<LoginView />}></Route>
+            <Route path='userview' element={<UserView />}>
+            </Route>
+          </Routes>
+      
+          
+        
+        </StyledContainer>
+      </Router>
+      
     </div>
+    
+    
   );
 }
-
 export default App;
