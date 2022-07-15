e885b72cc3ebadf61d971391244bba8d52ba2a7a
Huyen Vi <100335881+huyenvi237@users.noreply.github.com>
2022-07-15 15:11:13 +0900

    update App file
    SendEmailページ追加

--- lib_sys/src/App.js
+++ lib_sys/src/App.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useState } from 'react';
 
 import { 
   BrowserRouter as Router,
@@ -15,35 +13,69 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import LoginView from './pages/LoginView';
 
 //Test view
-import MemberInfoView from './pages/MemberInfoView';
+
+import SendMail from './pages/SendMail';
 import RenewPasswordView from './pages/RenewPasswordView';
 import UserView from './pages/UserView';
 import ManagerView from './pages/ManagerView';
+import MemUserView from './pages/MemUserView';
+import { useNavigate } from "react-router-dom";
+//import TestPage from './pages/TestPage';
+import CheckInfo from './pages/CheckInfo';
 
-
-function App() {
+/*function App() {
   return (
     <div>
-      <Router>
+      <TestPage />
+    </div>
+  )
+}
+
+export default App;
+*/
+
+
+
+function Root () {
+  const navigate = useNavigate();
+  useEffect(() =>  {
+    window.addEventListener("popstate", () => {
+      navigate(1);
+    });
+  })
+
+  return (
+    <div>
         <StyledContainer>
           <Routes>
-            <Route index path ='/' element={<LoginView />}></Route>
+            <Route index path ='/' element={<LoginView/>}></Route>
+
             <Route path='userview' element={<UserView />}></Route>
+            <Route path='memuserview' element={<MemUserView />}></Route>
+
             <Route path='manager' element={<ManagerView />}></Route>
-            <Route path='meminfo' element={<MemberInfoView />}></Route>
+            <Route path='resetinfo' element={<SendMail />}></Route>
+            <Route path='info' element={<CheckInfo />}></Route>
             <Route path='passchange' element={<RenewPasswordView />}></Route>
             
           </Routes>
       
-          
-        
         </StyledContainer>
-      </Router>
-      
     </div>
-    
-    
+  
   );
+}
+function App () {
+  return (
+    <Router>
+      <Root />
+    </Router>
+  )
+  
 }
 export default App;
 
+/*
+  - Code du tru:
+    <Route path='userview' element={<UserView />}></Route>
+*/ 
